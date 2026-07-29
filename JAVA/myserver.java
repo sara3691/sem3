23759dd0e1fce9deec732928f4cc6e5a2f073cc5
sara3691 <sarath6805@gmail.com>
2026-07-29 14:07:00 +0530

    webtech ex4

---
+++ JAVA/myserver.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class myserver {
+    public static void main(String[] args) throws Exception {
+            ServerSocket ss=new ServerSocket(3333);
+            Socket s=ss.accept();
+            DataInputStream din=new DataInputStream(s.getInputStream());
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            String str="",str2="";
+            while (!str.equals("stop"))
+            {
+
+                str=din.readUTF();
+                System.err.println("client says : "+str);
+                str2=br.readLine();  
+                dout.writeUTF(str2);  
+                dout.flush(); 
+            }
+
+            dout.close();
+            s.close();
+            ss.close();
+            
+        }
+    
+}
